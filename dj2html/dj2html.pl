7992e2969a6d215687c547ec74bbebf949d151a4
WellRedPandit <WellRedPandit@users.noreply.github.com>
2014-10-21 13:37:43 -0400

    examples and comments

--- dj2html/dj2html.pl
+++ dj2html/dj2html.pl
@@ -22,23 +22,22 @@ main();
 #---------------------------------------------
 
 sub main {
-  my $dj = join("\n",fin($opts{in}));
-  $dj =~ s#\r?\n\r?\n#<br/><br/>#mg;
-  $dj =~ s#r?\n# #mg;
-  $dj = comref($dj);
-  fout($opts{out}, [$dj]);
+  my $dj = join("<br/>",fin($opts{in})); # reads the file in whole
+  $dj = comref($dj); # separates comments from main text; creates IDs and crossrefs; formats html
+  fout($opts{out}, [$dj]); # write html out
 }
 
 sub comref {
   my $text = shift;
+  # initializes main text (head) and comments (tail)
   my ($head,$tail) = ("<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\"><h2>=== MAIN TEXT ===</h2>", "<h2>=== COMMENTS ===</h2>");
-  while( $text =~ /\[\[(.*?)\]\]/ ) {
-    $head .= $`;
-    my $comment = $1;
-    my ($txt,$refs) = mk_refs($comment);
+  while( $text =~ /\[\[(.*?)\]\]/ ) { # searches for the next comment between [[ and ]]
+    $head .= $`; # keeps the text before the comment (PREMATCH)
+    my $comment = $1; # remembers the value of the regex group above
+    my ($txt,$refs) = mk_refs($comment); # separates text (before the first |) from the comments; returns both, formatted
     $head .= $txt;
     $tail .= $refs;
-    $text = $'
+    $text = $' # takes what's left after the comment (POSTMATCH)
   }
   $head .= $text;
   $head . $tail
@@ -47,11 +46,11 @@ sub comref {
 sub mk_refs {
   my $comment = shift;
   my @coms = split(/\|/, $comment);
-  my $body = shift @coms;
+  my $body = shift @coms; # the fragment of the main text before the first comment
   my $txt = qq(<a name="txt_$txtid">$body</a>);
   my $ref = qq(<p>);
   my $comidx = 1;
-  for my $com ( @coms ) {
+  for my $com ( @coms ) { # makes crossrefs out of the text fragment and the comments
     $txt .= qq( <sup><a href="#com_${comid}_$comidx">[$comid.$comidx]</a></sup>);
     $ref .= qq(<a name="com_${comid}_$comidx">[$comid.$comidx]</a> <a href="#txt_$txtid">^</a> $com</br>);
     $comidx++;
@@ -63,6 +62,7 @@ sub mk_refs {
 
 #---------------------------------------------
 
+# reads a whole file into an array of lines
 sub fin {
   open my $fh, "<", $_[0] or die "!$!\n";
   my @lines = readline($fh);
@@ -70,6 +70,7 @@ sub fin {
   return map { chomp; $_ } @lines
 }
 
+# outputs an array of lines into a file
 sub fout { fw($_[0],$_[1],'>') }
 
 sub fw {
